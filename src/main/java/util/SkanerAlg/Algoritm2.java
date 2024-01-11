dfc3f04191da4ba6055b2dd144de4cd4adeb3a7e
OksanaPovkh <133179018+OksanaPovkh@users.noreply.github.com>
2024-01-11 17:34:54 -0500

    Added Algoritm2

---
+++ src/main/java/util/SkanerAlg/Algoritm2.java
@@ -0,0 +1,19 @@
+package util.SkanerAlg;
+
+public class Algoritm2 {
+    // уровень сложности: Написать псевдокод, блок-схему или текстовое описание для алгоритма: сложить три числа и вывести сумму.
+    //Реализовать алгоритм в коде.
+    public static void main(String[] args) {
+
+        int number1 = 10;
+        int number2 = 20;
+        int number3 = 30;
+
+        int sum = number1 + number2;
+        sum += number3;
+
+        System.out.println(sum);
+
+
+    }
+}
