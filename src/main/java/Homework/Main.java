fa694ab9c1d248473e3d7207dc627ea81f7680a7
OksanaPovkh <133179018+OksanaPovkh@users.noreply.github.com>
2024-01-12 18:19:30 -0500

    added main

--- src/main/java/Homework/Main.java
+++ src/main/java/Homework/Main.java
@@ -2,8 +2,18 @@ package Homework;
 
 public class Main {
     public static void main(String[] args) {
-        Person person1 = new Person("John", 25, 180, 80);
-        Person person2 = new Person("Mary", 35, 165, 55);
+        Person person1 = new Person("John", 25, 180, 80) {
+            @Override
+            public void die(int age) {
+
+            }
+        };
+        Person person2 = new Person("Mary", 35, 165, 55) {
+            @Override
+            public void die(int age) {
+
+            }
+        };
 
         person1.info();
         person1.goToWork();
